04e1904865707d57c47da1b1b199bb153de7984b
AntonioDMV <Antonio-DMV@outlook.com>
2023-01-16 22:48:16 -0500

    changes

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -10,24 +10,40 @@ const deteletUser = (user) => {
 }
 
 
+  const usersListOrd = userList.sort((a,b) => a.name?.localeCompare(b.name));
+
+
   return (
-    <div>
+    <div className='users-list' style={{
+      backgroundImage: `url(https://cdn.pixabay.com/photo/2013/08/28/12/03/plumage-176723_960_720.jpg)`,
+    }}>
+      
       <h1>Users</h1>
       <ul>
         {
           userList?.map(user => (
             <li key={user.id}>
-              <div>
-                <h2>{user.first_name} {user.last_name}</h2>
-                <p>{user.email}</p>
-                <p>{user.password}</p>
-                <p><i className="fa-solid fa-cake-candles"></i> {user.birthday}</p>
-                <button onClick={() => update(user)}>
-                  <i className="fa-sharp fa-solid fa-pen"></i>
-                </button>
-                <button onClick={() => deteletUser(user)}>
-                  <i className="fa-solid fa-trash"></i>
-                </button>
+              <div className='users-card'>
+                <div className='users-information'>
+
+                  <h2>{user.first_name} {user.last_name}</h2>
+                  <p>{user.email}</p>
+                  <p>{user.password}</p>
+                  <p><i className="fa-solid fa-cake-candles"></i> {user.birthday}</p>
+
+                </div>
+                
+                <div className='users-button'>
+
+                  <button className='edit-button' onClick={() => update(user)}>
+                    <i className="fa-sharp fa-solid fa-pen"></i>
+                  </button>
+                  <button className='delete-button' onClick={() => deteletUser(user)}>
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+
+                </div>
+                
               </div>
             </li>
           ))
--- src/components/UsersForm.jsx
+++ src/components/UsersForm.jsx
@@ -6,23 +6,30 @@ const UsersForm = ({ getUsers,  update2, update }) => {
 
     const { handleSubmit, register, reset } = useForm()
 
+    const emptyForm = {first_name: '', last_name: '', email: '', password: '', birthday: '' }
+
 useEffect(() => {
-  if (update2 !== null){
+  if (update2){
     reset(update2)
+  }else {
+    reset(emptyForm)
   }
 }, [update2])
 
     const upLoad = (data) => { 
      if(update2){
       axios.put(`https://users-crud.academlo.tech/users/${update2.id}/`,data)
-      .then(res => { 
+      .then(() => { 
         getUsers()
         update()
     }) //es es para cuando quiera agregar no me siga reemplazando, si que añada otro
      
     } else {
      axios.post('https://users-crud.academlo.tech/users/', data) //el post y put lleva un body que en este caso es data
-                .then(res => getUsers())
+                .then(() => {
+                    getUsers()
+                    reset(emptyForm)
+                });
      }
     }
 
@@ -32,50 +39,76 @@ useEffect(() => {
                 <div className='users-container'>
                     <h1>New user</h1>
                     <br />
-                    <div className='names'>
-                        <div className='user-container'>
-                            <label htmlFor="first_name">First Name</label>
+                    <div className='names-container'>
+                        <i className="fa-solid fa-user"></i>
+                        <div className='name'>
+                            <label htmlFor="first_name"></label>
                             <input type="text"
+                            placeholder='First name'
                                 id='first_name'
                                 required
                                 {...register("first_name")}
                             />
                         </div>
-                        <div className='user-container'>
+                        <div className='last-name'>
+                            <label htmlFor="last_name"></label>
+                            <input type="text"
+                            placeholder='Last name'
+                            required
+                                id='last_name'
+                                {...register("last_name")}
+                            
+                            />
+                        </div>
+                            
+                        
+                        {/* <div className='user-container'>
                             <label htmlFor="last_name">Last Name</label>
                             <input type="text"
                              required
                                 id='last_name'
                                 {...register("last_name")}
                             />
-                        </div>
+                        </div> */}
                     </div>
-                    <div className='email'>
+                    <div className='mail-container'>
+                        <i className="fa-solid fa-envelope"></i>
                         <div className='user-container'>
-                            <label htmlFor="email">email</label>
+                            <label htmlFor="email"></label>
                             <input type="text"
+                            placeholder='Email'
                              required
                                 id='email'
                                 {...register("email")}
                             />
                         </div>
+                    </div>    
+                    <div className='password-container'>
+                        <i className="fa-solid fa-lock"></i>
                         <div className='user-container'>
-                            <label htmlFor="password">Password</label>
+                        
+                            <label htmlFor="password"></label>
                             <input type="password"
+                            placeholder='Password'
                              required
                                 id='password'
                                 {...register("password")}
                             />
                         </div>
+                    </div>
+                    <div className='birthday-container'>
+                        <i className="fa-solid fa-cake-candles"></i>
                         <div className='user-container'>
-                            <label htmlFor="birthday">Birthday</label>
+                            <label htmlFor="birthday"></label>
                             <input type="date"
                              required
                                 id='birthday'
                                 {...register("birthday")}
                             />
                         </div>
-                    </div>
+                    </div>    
+                        
+                    
                     <button> Upload </button>
                     {/* como estamos trabajando con formulario no utilizamos onClick */}
                 </div>
